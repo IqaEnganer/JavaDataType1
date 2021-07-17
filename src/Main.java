717fa6747d24f220bed50f84d28d4a242eec0907
iqaenganer <daw.camalian@yandex.ru>
2021-07-17 23:30:57 +0300

    1com

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 8799;
+        int Quantity = 6;
+        int totalcost = ticket * Quantity;
+        int each = 20;
+        int Bonusrubles = totalcost / each;
+
+        System.out.println(Bonusrubles);
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной милли
+
+        // Рассчитываете количество бонусных милль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
